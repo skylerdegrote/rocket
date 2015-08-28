dd35a8f7110bf44e0e5786e9293ad6b3fcbf4605
Skyler <skyler.m.degrote@outlook.com>
2015-08-28 11:15:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Customize Your Rocket
+
+This application will let you choose a nose cone, choose the size of the body tube, flip through different fin options and change out the engine to your rocket. Then you'll be able to choose a color for each of these parts and save your customized rocket. Find out what pieces you need to go buy to create your rocket in real life too!
---
+++ server/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var path = require('path');
+
+router.post('/', passport.authenticate('local', {
+    successRedirect: '../views/users.html',
+    failureRedirect: '/'
+}));
+
+router.get("/*", function(req, res, next){
+    var file = req.params[0] || "views/index.html";
+    res.sendFile(path.join(__dirname, "../public", file));
+});
+
+module.exports = router;
