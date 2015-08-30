ab7565871c315086b84f759866456db9db254ab3
Skyler <skyler.m.degrote@outlook.com>
2015-08-30 10:48:00 -0500

    images fixed and lined up

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -22,21 +22,6 @@ app.controller("mainRocketController", ["$scope",  function($scope) {
     // write code that will switch on click to half circle nose cone
 
 
-    //    $scope.followBtnImgUrl = '/sth.jpg';
-    //
-    //$scope.noseCones = [{imgUrl: "rocketpieces_pointed-nosecone.png", name:"pointed nose"},
-    //        {imgUrl: "rocketpieces_rounded-nosecone.png", name: "rounded nose"},
-    //        {imgUrl: "rocketpieces_halfcircle-nosecone.png", name: "half-circle-nose"}];
-    //
-    //
-    //    $scope.toggleImage = function(nosecone) {
-    //        if(nosecone.imgUrl === $scope.followBtnImgUrl) {
-    //            nosecone.imgUrl = nosecone.$backupUrl;
-    //        } else {
-    //            nosecone.$backupUrl = merchant.imgUrl;
-    //            nosecone.imgUrl = $scope.followBtnImgUrl;
-    //        }
-    //    };
     }]);
 
 
