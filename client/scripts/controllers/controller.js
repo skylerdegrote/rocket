081656224fd00d25a1373b9ad1b90b9d538a651e
Skyler <skyler.m.degrote@outlook.com>
2015-08-30 16:31:45 -0500

    trying to get modal to work

--- client/scripts/controllers/controller.js
+++ client/scripts/controllers/controller.js
@@ -4,25 +4,15 @@
 var app = angular.module("rocketApp", []);
 
 
-app.controller("mainRocketController", ["$scope",  function($scope) {
-
-    $scope.noseCones = [{imgUrl: "rocketpieces_pointed-nosecone.png", name:"pointed nose"},
-        {imgUrl: "rocketpieces_rounded-nosecone.png", name: "rounded nose"},
-        {imgUrl: "rocketpieces_halfcircle-nosecone.png", name: "half-circle-nose"}];
-
-
-
+app.controller("mainRocketController", ["$scope", function($scope) {
+        //$scope.noseconeinfo = infoboxservice.modal;
+        $scope.noseconeinfo = function(){
+                return ("hello");
+    }
+}]);
 
 
-    // write code that will switch on click to pointed nose cone
-        $scope.pointednosecone = function(nosecone){
-              nosecone.imgUrl = $scope.noseCones[0]
-    };
-    // write code that will switch on click to rounded nose cone
-    // write code that will switch on click to half circle nose cone
 
-
-    }]);
 
 
 
